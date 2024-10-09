b6af820e66aef612bb1472e68df5f5f8a7bba213
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2024-10-09 14:00:37 +0000

    go

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-      domains: ['res.cloudinary.com','img.freepik.com'], // Add both domains here
+      domains: ['res.cloudinary.com'], // Add both domains here
     },
   };
   
