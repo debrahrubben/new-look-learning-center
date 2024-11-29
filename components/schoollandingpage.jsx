342626bb89d3117815cd1b9d74e553421675237a
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2024-11-29 11:33:41 +0000

    Update schoollandingpage.jsx

--- components/schoollandingpage.jsx
+++ components/schoollandingpage.jsx
@@ -72,7 +72,7 @@ export function SchoolLandingPageJsx() {
                 At New Look Learning Centre (NLLC) our efforts are
 geared towards the provision of a holistic education
 in a conducive. academic , excellent and healthy
-envronment for pre-school children in Kumasi, Ghana.
+envronment for pre-school children in Kumasi in the ashanti region of Ghana
 and beyond through exceeding good practices.
                 </p>
               </div>
